278a29474013b6340dd0a97e0192bc5f9ac0f84a
Protiuns <newton281992@gmail.com>
2023-07-12 21:30:01 -0300

    0001

--- Scripts/Monstros/Monstro.gd
+++ Scripts/Monstros/Monstro.gd
@@ -4,7 +4,15 @@ extends CharacterBody2D
 const velocidade = 75
 
 func _physics_process(delta):
-	var dir =  $"../Jogador".position - position
-	velocity = dir.normalized() * velocidade
+	
 
 	move_and_slide()
+func Perseguindo():
+	var dir =  $"../Jogador".position - position
+	velocity = dir.normalized() * velocidade
+func Parado():
+	velocity = Vector2.ZERO
+func Machucado():
+	pass
+func Patrulhando():
+	pass
