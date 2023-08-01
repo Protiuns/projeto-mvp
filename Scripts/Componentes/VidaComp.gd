0b1327947251c9bf5fa0b969163c1821a05b2cff
Protiuns <newton281992@gmail.com>
2023-08-01 17:40:38 -0300

    Tururun

---
+++ Scripts/Componentes/VidaComp.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name VidaComp
+
+@export var vidaMaxima := 10.0
+var vida: float
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	vida= vidaMaxima
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func dano():
+	pass
