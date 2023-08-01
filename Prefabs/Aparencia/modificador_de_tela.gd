97a4a3fa496a525666d1877271d10f54c4f01a64
Protiuns <newton281992@gmail.com>
2023-08-01 13:37:21 -0300

    Olha as coisas se encaminhando

---
+++ Prefabs/Aparencia/modificador_de_tela.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+
+var maxScrol =0.01 
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	print(get_canvas_item())  
